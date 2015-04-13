f08c89d0f0f9755a34c42fab78c2834aae69533c
scofieldWyq <794870409@qq.com>
2015-04-13 11:34:45 +0800

    add some note

--- Linux_pwd.c
+++ Linux_pwd.c
@@ -24,26 +24,32 @@ int main()
 void path_up_to_father( ino_t current_inode)
 {
 
-	ino_t this_inode;
+	ino_t this_inode;			/* this is used for next directory */
 
-	char this_dir_name[BUFSIZ];
+	char this_dir_name[BUFSIZ];		/* set a string to save cuurrent directory name */
 
-	if( get_inode("..") != current_inode)
+	if( get_inode("..") != current_inode)	/* if this direcory inode is the same as father inode, it is get to the root directory */
 	{
-		chdir("..");
+		chdir("..");			/* cause you can't know the directory name by itself, you should change directory to father, and compare with the inode to find the directory name */
+
 		inode_to_file_name( this_dir_name, current_inode, BUFSIZ);
+						/* get directory by inode */
 		this_inode = get_inode(".");
-		path_up_to_father( this_inode );
-		printf("/%s", this_dir_name);
+		path_up_to_father( this_inode ); /* resursively */
+
+		printf("/%s", this_dir_name);	/* prints directory name */
 	}
 }
 
 void inode_to_file_name( char * current_file_name, ino_t current_inode, int str_len)
+/*
+ * change inode into curect file name.
+ */
 {
 	DIR *dir_ptr;
 	struct dirent *direntry_ptr;
 
-	dir_ptr = opendir( ".");
+	dir_ptr = opendir( ".");	/* open dir */
 
 	if( dir_ptr == NULL )
 	{
@@ -51,29 +57,34 @@ void inode_to_file_name( char * current_file_name, ino_t current_inode, int str_
 		exit(1);
 	}
 
-	while( ( direntry_ptr = readdir(dir_ptr)) != NULL )
+	while( ( direntry_ptr = readdir(dir_ptr)) != NULL ) /* read directory until got the curect inode or not */
 	{
 		if( get_inode(direntry_ptr->d_name) == current_inode )
 		{
-			strncpy( current_file_name, direntry_ptr->d_name, str_len);
-			closedir(dir_ptr);
+			strncpy( current_file_name, direntry_ptr->d_name, str_len); /* copy directory name to current file name */
+			closedir(dir_ptr); /* close dir */
 			return ;
 		}
 	}
 }
 
 ino_t get_inode( char * fname)
+/*
+ * return inode number
+ */
+
 {
-	struct stat f_info;
+	struct stat f_info;	/* file property structure */
 
 	if (stat( fname, &f_info) == -1 )
+	/* return -1 means that failed. */
 	{
 		fprintf(stderr, "Cannot stat!\n");
 		perror(fname);
 		exit(1);
 	}
 
-	return f_info.st_ino;
+	return f_info.st_ino;	/* return inode */
 }
 
 
